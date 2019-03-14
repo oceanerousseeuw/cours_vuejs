2e774ccef658570f51dc1c6f02996d97995c04aa
oceane <oceane.rousseeuw@gmail.com>
2019-03-14 16:01:18 +0100

    suite liste course

--- listecourse.js
+++ listecourse.js
@@ -2,29 +2,39 @@ new Vue({
     el : "#app",
     data : {
         element : '',
-        list:[
-            //{text: "tomate", price: 0, good: false}
-        ],
-        total: 0
+        list:[],
+        budget: 0
+    },
+    computed : {
+        getTotal : function(){
+            let total = 0;
+            for (var i = 0; i < this.list.length; i++) {
+                if(this.list[i].good){
+                    total += Number(this.list[i].price);
+                }
+            }
+            return total;
+        },
+
+        getBudgetMessage : function(){
+            let total = this.getTotal;
+            if(this.budget < total){
+                return "Tu dépasses le budget";
+            }else{
+                return "";
+            }
+        }
     },
     methods: {
         addOnList: function(){
-            this.list.push({text :this.element, price:this.price, good:false});
+            this.list.push({text :this.element, price:"", good:false});
         },
         addOnBag: function(item){
-            for (var i = 0; i < this.list.length; i++) {
+            for (let i = 0; i < this.list.length; i++) {
                 if (this.list[i].text === item.text) {
                     this.list[i].good = true;
-                    console.log(item);
-                    this.list[i].price = Number(item.price);
                 }
             }
-            /*for (var i = 0; i < this.list.length; i++) {
-                this.total = 0;
-                if(this.list[i].good){
-                    this.total += Number(this.list[i].price);
-                }
-            }*/
         },
         deleteFromList: function(index){
             this.list.splice(index, 1);
